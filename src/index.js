f7a8e6a3bbefa5f4c43fe4048c89fad37d4f7dda
Tushar saini <ttushar476@gmail.com>
2024-04-03 10:20:58 +0530

    old

--- src/index.js
+++ src/index.js
@@ -1,29 +1,20 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
-import express from "express";
 import connectDB from "./db/index.js";
 import {app} from './app.js'
-// dotenv.config({
-//     path: './.env'
-// })
-const ap = express()
-ap.use("/", (req, res) => {
-    res.json({ message: "hello world from expres"})
-});
-
-ap.listen(9000, () => {
-    console.log("app started successfully");
+dotenv.config({
+    path: './.env'
 })
 
-// connectDB()
-// .then(() => {
-//     app.listen(process.env.PORT || 8000, () => {
-//         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-//     })
-// })
-// .catch((err) => {
-//     console.log("MONGO db connection failed !!! ", err);
-// })
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
 
 
 
